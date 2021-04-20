d28be593fcda2efa3227968d595d41f4dc3a433d
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-04-20 18:32:16 +0200

    update

--- _Code/04/Basics_Hello_World_Blink/src/main.cpp
+++ _Code/04/Basics_Hello_World_Blink/src/main.cpp
@@ -37,16 +37,16 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(21, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  digitalWrite(21, HIGH);
+  digitalWrite(12, HIGH);
   Serial.println("on");
   delay(750);
-  digitalWrite(21, LOW);
+  digitalWrite(12, LOW);
    Serial.println("off");
   delay(750);
 }
